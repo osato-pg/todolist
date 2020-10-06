b392b37e7bc4355cdf00a7c59ee88d057c22d5aa
osato-pg <ee2825st@gmail.com>
2020-10-06 20:55:36 +0900

    add label function

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <h1>To Do リスト</h1>
-      <form>
-      <input type="radio" name="status" value="all" checked />全て
-      <input type="radio" name="status" value="middle" />作業中
-      <input type="radio" name="status" value="done" />完了
+    <form>
+      <label v-for="label in options" :key="label.value">
+        <input type="radio" v-model="current" v-bind:value="label.value" />
+        {{ label.label }}
+      </label>
     </form>
     <table>
       <tr>
@@ -12,10 +13,12 @@
         <th>コメント</th>
         <th>状態</th>
       </tr>
-      <tr v-for="(todo, index) in todos" :key="todo">
-        <td>{{index}}</td>
-        <td>{{todo.text}}</td>
-        <td><button @click="doneTask(index)">{{todo.done}}</button></td>
+      <tr v-for="(todo, index) in filteredTodos" :key="todo">
+        <td>{{ index }}</td>
+        <td>{{ todo.text }}</td>
+        <td>
+          <button @click="stateTask(index)">{{ labels[todo.state] }}</button>
+        </td>
         <td><button @click="deleteTask(index)">削除</button></td>
       </tr>
     </table>
@@ -31,24 +34,41 @@ export default {
     return {
       addText: "",
       todos: [],
+      current: -1,
+      options: [
+        { value: -1, label: "すべて" },
+        { value: 0, label: "作業中" },
+        { value: 1, label: "完了" },
+      ],
     };
+  },
+  computed: {
+    filteredTodos() {
+      return this.todos.filter(function (todo) {
+        return this.current < 0 ? true : this.current === todo.state;
+      }, this);
+    },
+    labels() {
+      return this.options.reduce(function (a, b) {
+        return Object.assign(a, { [b.value]: b.label });
+      }, {});
+    },
   },
   methods: {
     addTask() {
-      this.todos.push({ text: this.addText, done: '作業中' });
+      this.todos.push({ text: this.addText, state: 0 });
       this.addText = "";
     },
     deleteTask(index) {
       this.todos.splice(index, 1);
     },
-    doneTask(index) {
-      if(this.todos[index].done==='作業中'){
-        this.todos[index].done='完了';
-      }else {
-        this.todos[index].done='作業中';
+    stateTask(index) {
+      if (this.todos[index].state === 0) {
+        this.todos[index].state = 1;
+      } else {
+        this.todos[index].state = 0;
       }
-      
-    }
+    },
   },
 };
 </script>
